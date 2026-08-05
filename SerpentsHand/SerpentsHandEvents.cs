2130dbc06f72d17f3786eabb331dc94e310800e1
Herzog-XI <egmherzog@gmail.com>
2026-08-05 16:58:36 +0200

    Trigger Serpent's Hand after second respawn wave

--- SerpentsHand/SerpentsHandEvents.cs
+++ SerpentsHand/SerpentsHandEvents.cs
@@ -1,5 +1,7 @@
 using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Server;
 using Player = Exiled.Events.Handlers.Player;
+using Server = Exiled.Events.Handlers.Server;
 
 namespace FacilityJobs.SerpentsHand
 {
@@ -8,16 +10,32 @@ namespace FacilityJobs.SerpentsHand
         public void Register()
         {
             Player.Left += OnLeft;
+            Server.RespawningTeam += OnRespawningTeam;
         }
 
         public void Unregister()
         {
             Player.Left -= OnLeft;
+            Server.RespawningTeam -= OnRespawningTeam;
         }
 
         private static void OnLeft(LeftEventArgs ev)
         {
             SerpentsHandManager.Remove(ev.Player);
+        }
+
+        private static void OnRespawningTeam(RespawningTeamEventArgs ev)
+        {
+            if (!RoundState.SerpentsHandSelected || RoundState.SerpentsHandSpawned)
+                return;
+
+            RoundState.RespawnWaveCount++;
+
+            if (Plugin.Instance.Config.Debug)
+                Exiled.API.Features.Log.Debug($"[FacilityJobs] Normal respawn wave #{RoundState.RespawnWaveCount} detected.");
+
+            if (RoundState.RespawnWaveCount == 2)
+                Plugin.Instance.SerpentsHandSpawnManager.ScheduleAfterSecondWave();
         }
     }
 }
