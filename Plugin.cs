d31c09143249de99f5ac0899ab1054a331d6a9b2
Herzog-XI <egmherzog@gmail.com>
2026-08-05 16:41:23 +0200

    Register CI Agent event handlers

--- Plugin.cs
+++ Plugin.cs
@@ -10,23 +10,31 @@ namespace FacilityJobs
 
         public override string Name => "FacilityJobs";
         public override string Author => "Herzog-XI";
-        public override Version Version => new Version(0, 1, 0);
+        public override Version Version => new Version(0, 2, 0);
         public override Version RequiredExiledVersion => new Version(9, 14, 2);
 
         private RoundEvents roundEvents;
+        private CiAgentEvents ciAgentEvents;
 
         public override void OnEnabled()
         {
             Instance = this;
+
             roundEvents = new RoundEvents();
+            ciAgentEvents = new CiAgentEvents();
+
             roundEvents.Register();
+            ciAgentEvents.Register();
 
             base.OnEnabled();
         }
 
         public override void OnDisabled()
         {
+            ciAgentEvents?.Unregister();
             roundEvents?.Unregister();
+
+            ciAgentEvents = null;
             roundEvents = null;
             Instance = null;
 
