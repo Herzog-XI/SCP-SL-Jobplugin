b8955f8b1bea52ed15b59c62873afd742125c381
Herzog-XI <egmherzog@gmail.com>
2026-08-05 16:37:24 +0200

    Assign round-start jobs after player roles spawn

--- Events/RoundEvents.cs
+++ Events/RoundEvents.cs
@@ -1,10 +1,14 @@
 using Exiled.API.Features;
+using FacilityJobs.Managers;
+using MEC;
 using Server = Exiled.Events.Handlers.Server;
 
 namespace FacilityJobs.Events
 {
     internal sealed class RoundEvents
     {
+        private CoroutineHandle assignmentCoroutine;
+
         public void Register()
         {
             Server.WaitingForPlayers += OnWaitingForPlayers;
@@ -17,24 +21,39 @@ namespace FacilityJobs.Events
             Server.WaitingForPlayers -= OnWaitingForPlayers;
             Server.RoundStarted -= OnRoundStarted;
             Server.RestartingRound -= OnRestartingRound;
+
+            CancelAssignment();
         }
 
-        private static void OnWaitingForPlayers()
+        private void OnWaitingForPlayers()
         {
+            CancelAssignment();
             RoundState.Reset();
         }
 
-        private static void OnRoundStarted()
+        private void OnRoundStarted()
         {
             RoundState.Initialize();
 
             if (Plugin.Instance.Config.Debug)
                 Log.Debug($"[FacilityJobs] Round initialized. Starting SCPs: {RoundState.StartingScpCount}.");
+
+            CancelAssignment();
+            assignmentCoroutine = Timing.CallDelayed(1.5f, JobManager.AssignRoundStartJobs);
         }
 
-        private static void OnRestartingRound()
+        private void OnRestartingRound()
         {
+            CancelAssignment();
             RoundState.Reset();
+        }
+
+        private void CancelAssignment()
+        {
+            if (assignmentCoroutine.IsRunning)
+                Timing.KillCoroutines(assignmentCoroutine);
+
+            assignmentCoroutine = default;
         }
     }
 }
